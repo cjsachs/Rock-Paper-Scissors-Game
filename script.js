e3d1c923011856f1d5ed50ab3d749df252cb715b
byaskew3 <byaskew3@gmail.com>
2021-12-18 23:30:20 -0700

    created function for computer

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello World');
+
+function computerPlay() { // will generate a random number between 1-3. 1 = 'Rock', 2 = 'Paper', 3 = 'Scissors'
+    let picker = Math.floor(Math.random()*3) + 1;
+    if (picker == 1) {
+        picker = 'Rock';
+        return picker;
+    } else if (picker == 2) {
+        picker = 'Paper';
+        return picker;
+    } else {
+    picker = 'Scissors';
+    return picker;
+    }
+};
+
+function playGame(playerSelection, computerSelection) {
+    
+}
