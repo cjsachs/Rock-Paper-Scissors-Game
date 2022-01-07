ca33a0588ebbcc54c7db6b3d4b38e804405f2f8a
byaskew3 <byaskew3@gmail.com>
2022-01-07 16:18:07 -0700

    added UI and cleaned up code

--- script.js
+++ script.js
@@ -1,82 +1,74 @@
 let playerScore = 0;
 let computerScore = 0;
 
-//function computerPlay() will generate a random number between 1-3. 1 = 'Rock', 2 = 'Paper', 3 = 'Scissors'
-function computerPlay() { 
-    let picker = Math.floor(Math.random()*3) + 1;
-    if (picker == 1) {
-        picker = 'Rock';
-        return picker;
-    } else if (picker == 2) {
-        picker = 'Paper';
-        return picker;
-    } else {
-    picker = 'Scissors';
-    return picker;
-    }
+const playerScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('cpu-score');
+const scoreBoard_div = document.querySelector('.score-board');
+const result_p = document.querySelector('.result p');
+const rock_div = document.getElementById('Rock');
+const paper_div = document.getElementById('Paper');
+const scissors_div = document.getElementById('Scissors');
+
+function computerPlay() {
+    const choices = ['Rock', 'Paper', 'Scissors']
+    let picker = Math.floor(Math.random()*3);
+    return choices[picker];
 };
 
-// function playGame() will compare the players choice with the CPU's choice to decide who is the winner and loser!
+function win(userChoice, cpuChoice) {
+    const userChoice_div = document.getElementById(userChoice) 
+    playerScore++;
+    playerScore_span.innerHTML = playerScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${userChoice.toUpperCase()}(you) beats ${cpuChoice.toUpperCase()}(cpu). You Win!`
+    userChoice_div.classList.add('green-glow');
+    setTimeout(() => userChoice_div.classList.remove('green-glow'), 400)
+}
+function lose(userChoice, cpuChoice) {
+    const userChoice_div = document.getElementById(userChoice) 
+    computerScore++;
+    playerScore_span.innerHTML = playerScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `${userChoice.toUpperCase()}(you) loses to ${cpuChoice.toUpperCase()}(cpu). You Lose!`
+    userChoice_div.classList.add('red-glow');
+    setTimeout(() => userChoice_div.classList.remove('red-glow'), 400)
+}
+function tie(userChoice, cpuChoice) {
+    const userChoice_div = document.getElementById(userChoice)
+    playerScore_span.innerHTML = playerScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = `Both of you chose ${userChoice.toUpperCase()}. Tie Game!`
+    userChoice_div.classList.add('gray-glow');
+    setTimeout(() => userChoice_div.classList.remove('gray-glow'), 400)
+}
+
+function game(userChoice) {
+    const cpuChoice = computerPlay();
+    switch (userChoice + cpuChoice) {
+        case 'RockScissors':
+        case 'PaperRock':
+        case 'ScissorsPaper':
+            win(userChoice, cpuChoice);
+            break;
+        case 'RockPaper':
+        case 'PaperScissors':
+        case 'ScissorsRock':
+            lose(userChoice, cpuChoice);
+            break;
+        case 'RockRock':
+        case 'PaperPaper':
+        case 'ScissorsScissors':
+            tie(userChoice, cpuChoice);
+            break;
+    }
+}
+
 function playGame() {
-    let playerSelection = prompt('Choose wisely... Rock, Paper, or Scissors?');
-    const computerSelection = computerPlay();
-    playerSelection = playerSelection.toLowerCase();
-    // Code IF player chooses ROCK as their choice
-    if (playerSelection == 'rock' && computerSelection == 'Rock') {
-        console.log(`Tie Game! You chose ${playerSelection.replace('r', 'R')} And the CPU chose ${computerSelection}!`);
-        console.log(`The current score: You: ${playerScore} CPU: ${computerScore}`);
-    } else if (playerSelection == 'rock' && computerSelection =='Paper') {
-        console.log(`You Lose! You chose ${playerSelection.replace('r', 'R')} And the CPU chose ${computerSelection}!`);
-        computerScore += 1;
-        console.log(`The current score: You: ${playerScore} CPU: ${computerScore}`);
-    } else if (playerSelection == 'rock' && computerSelection =='Scissors') {
-        console.log(`You Win! You chose ${playerSelection.replace('r', 'R')} And the CPU chose ${computerSelection}!`);
-        playerScore += 1;
-        console.log(`The current score: You: ${playerScore} CPU: ${computerScore}`);
-    }
+    rock_div.addEventListener('click', () => game('Rock'));
+    
+    paper_div.addEventListener('click', () => game('Paper'));
 
-    // Code IF player choose PAPER as their choice
-    else if (playerSelection == 'paper' && computerSelection == 'Paper') {
-        console.log(`Tie Game! You chose ${playerSelection.replace('p', 'P')} And the CPU chose ${computerSelection}!`);
-        console.log(`The current score: You: ${playerScore} CPU: ${computerScore}`);
-    } else if (playerSelection == 'paper' && computerSelection =='Scissors') {
-        console.log(`You Lose! You chose ${playerSelection.replace('p', 'P')} And the CPU chose ${computerSelection}!`)
-        computerScore += 1;
-        console.log(`The current score: You: ${playerScore} CPU: ${computerScore}`);
-    } else if (playerSelection == 'paper' && computerSelection =='Rock') {
-        console.log(`You Win! You chose ${playerSelection.replace('p', 'P')} And the CPU chose ${computerSelection}!`)
-        playerScore += 1;
-        console.log(`The current score: You: ${playerScore} CPU: ${computerScore}`);
-    }
+    scissors_div.addEventListener('click', () => game('Scissors'));
+}
 
-    // Code IF player choose SCISSORS as their choice
-    else if (playerSelection == 'scissors' && computerSelection == 'Scissors') {
-        console.log(`Tie Game! You chose ${playerSelection.replace('s', 'S')} And the CPU chose ${computerSelection}!`);
-        console.log(`The current score: You: ${playerScore} CPU: ${computerScore}`);
-    } else if (playerSelection == 'scissors' && computerSelection =='Rock') {
-        console.log(`You Lose! You chose ${playerSelection.replace('s', 'S')} And the CPU chose ${computerSelection}!`)
-        computerScore += 1;
-        console.log(`The current score: You: ${playerScore} CPU: ${computerScore}`);
-    } else if (playerSelection == 'scissors' && computerSelection =='Paper') {
-        console.log(`You Win! You chose ${playerSelection.replace('s', 'S')} And the CPU chose ${computerSelection}!`)
-        playerScore += 1;
-        console.log(`The current score: You: ${playerScore} CPU: ${computerScore}`);
-    } else {
-        message = 'Oops something went wrong with our program! Try Again!'
-        return message;
-    }
-};
-
-// function game() creates a 5 round game and keeps score
-function game() {
-    while (playerScore < 5 && computerScore < 5) {
-        playGame();
-        if (computerScore == 5) {
-        console.log('The CPU WINS!')
-        } else if (playerScore == 5) {
-        console.log('You WIN!')
-        }
-    };
-};
-
-game();
+playGame();
